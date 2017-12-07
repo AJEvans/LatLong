fcaf36053018c7d4a17a9af842506799cc5be225
AJEvans <a.j.evans@leeds.ac.uk>
2017-12-07 01:10:58 +0000

    First push

---
+++ latlong.py
@@ -0,0 +1,174 @@
+
+class LatLong():
+
+    def __init__(self):
+        self.long_degrees = -1
+        self.long_direction = ""
+        self.long = ""
+        
+
+        
+        
+    def set_long(self, longitude):
+        """ Set the longitude
+        
+        Positional parameters:
+        longitude -- a strings of a number between "0" and "180" combined with "E" or "W", e.g. "10E"
+                
+        Raises:
+        NumberOutOfRangeError
+        DirectionScrewyError
+        
+        """
+        
+        
+        # If set erroneously, set defaults
+        degrees = ""
+        direction = ""
+        self.long_degrees = -1
+        self.long_direction = ""
+        self.long = str(degrees) + ""
+        
+        
+        
+        for character in longitude:
+            if character.isdigit():
+                degrees = degrees + character
+            else:
+                break
+        for character in longitude[len(degrees):]:  
+            if character.isalpha():
+                direction = direction + character
+            else:
+                break
+                
+        if len(degrees) == 0 or len(degrees) > 3:
+            raise NumberOutOfRangeError(longitude)
+        
+                
+        degrees = int(degrees)
+        if degrees < 0 or degrees > 180:
+            raise NumberOutOfRangeError(longitude)
+        
+        if len(direction) != 1:
+            raise DirectionScrewyError(longitude)
+ 
+ 
+        if direction != "E" and direction != "W":
+            raise DirectionScrewyError(longitude)
+
+            
+        self.long_degrees = degrees
+        self.long_direction = direction
+        self.long = str(degrees) + direction
+            
+
+            
+            
+    def get_long(self):
+        """Get the longitude.
+        
+        Returns:
+        longitude -- a strings of a number between "0" and "180" combined with "E" or "W", e.g. "10E"
+        
+        Raises:
+        NotSetError
+        
+        """
+        if self.long == "":
+            raise NotSetError("")
+        else:
+            return self.long
+        
+        
+        
+        
+    def get_long_degrees(self):
+        """Get the longitude degrees.
+        
+        Returns:
+        longitude -- int between 0 and 180.
+        
+        Raises:
+        NotSetError
+        
+        """
+        if self.long_degrees == -1:
+            raise NotSetError("")
+        else:
+            return self.long_degrees      
+         
+        
+        
+        
+    def get_long_direction(self):
+        """Get the longitude direction.
+        
+        Returns:
+        longitude -- int between 0 and 180.
+        
+        Raises:
+        NotSetError
+        
+        """
+        if self.long_direction == "":
+            raise NotSetError("")
+        else:
+            return self.long_degrees      
+
+
+
+        
+        
+class NumberOutOfRangeError(ValueError):
+    """Exception raised for errors with the number range for longitude.
+    
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message="Numbers should be between 0 and 180 for longitude."):
+        self.expression = expression
+        self.message = message
+    
+
+        
+        
+class DirectionScrewyError(ValueError):
+    """Exception raised for errors with the direction for longitude.
+    
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message="Direction should be either 'E' or 'W' for longitude."):
+        self.expression = expression
+        self.message = message
+
+       
+
+       
+class NotSetError(ValueError):
+    """Exception raised when value not set.
+    
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message="Value not set."):
+        self.expression = expression
+        self.message = message        
+        
+        
+        
+        
+if __name__ == '__main__':
+    # This is just a little bit if we want to run 
+    # the module as a script for testing during development
+    a = LatLong()
+    a.set_long("1000E")
+    print(a.get_long())
+
---
+++ testlatlong.py
@@ -0,0 +1,64 @@
+
+import unittest
+import latlong
+
+class TestLatLong(unittest.TestCase):
+
+
+    def test_set_long(self):
+        a = latlong.LatLong()
+        a.set_long("10E")
+        self.assertEqual(a.get_long(), "10E")
+        self.assertRaises(latlong.NumberOutOfRangeError, a.set_long, "1000E") 
+        self.assertRaises(latlong.NumberOutOfRangeError, a.set_long, "-1E")
+        self.assertRaises(latlong.DirectionScrewyError, a.set_long, "10R")
+        self.assertRaises(latlong.DirectionScrewyError, a.set_long, "10EE")
+        
+        
+        
+        
+    def test_get_long(self):
+        a = latlong.LatLong()
+        # Test for unset
+        self.assertRaises(latlong.NotSetError, a.get_long)
+        #Test for set
+        a.set_long("10E")
+        self.assertEqual(a.get_long(), "10E")
+        # Test for badly set resetting default
+        # Note as set_long throws an exception on bad arguments
+        # we need to try-except this.
+        try:
+            a.set_long("blah")
+        except:
+            self.assertRaises(latlong.NotSetError, a.get_long)
+        
+        
+        
+        
+    def test_get_long_degrees(self): 
+        a = latlong.LatLong()
+        self.assertRaises(latlong.NotSetError, a.get_long_degrees)
+        a.set_long("10E")
+        self.assertEqual(a.get_long_degrees(), 10)
+        try:
+            a.set_long("blah")
+        except:
+            self.assertRaises(latlong.NotSetError, a.get_long_degrees)
+        
+        
+        
+        
+    def test_get_long_direction(self): 
+        a = latlong.LatLong()
+        self.assertRaises(latlong.NotSetError, a.get_long_direction)    
+        a.set_long("10E")
+        self.assertEqual(a.get_long_direction(), 10)
+        try:
+            a.set_long("blah")
+        except:
+            self.assertRaises(latlong.NotSetError, a.get_long_direction)
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
